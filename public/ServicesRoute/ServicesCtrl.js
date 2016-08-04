e3dd9af48b4bc50e89467d6e01c8b9618aa2eadd
ColeHartog <agentcole7@gmail.com>
2016-08-04 15:11:56 -0600

    clean, clean, clean

--- public/ServicesRoute/ServicesCtrl.js
+++ public/ServicesRoute/ServicesCtrl.js
@@ -1,13 +1,13 @@
 angular.module("salono").controller("ServicesCtrl", ["$scope", "ServicesSvc", function($scope, ServicesSvc){
     
-    $scope.Selected = "Design + Shape";
+    $scope.Selected = "Design+Shape";
     
-    $scope.Services = ["Design + Shape", "Pigment", "Remedy + Restore", "Polish", "Texture", "Indulge"];
+    $scope.Services = ["Design+Shape", "Pigment", "Remedy+Restore", "Polish", "Texture", "Indulge"];
     
 	$scope.ServiceDetails = {
-        "Design + Shape": ["Women's Cuts", "Men's Cuts", "Shampoo / Blowdry", "Blowdry Lesson", "Hair Extensions: All hair extension appointments require a consultation first."],
+        "Design+Shape": ["Women's Cuts", "Men's Cuts", "Shampoo / Blowdry", "Blowdry Lesson", "Hair Extensions: All hair extension appointments require a consultation first."],
         "Pigment": ["Full Color", "Retouch Color", "Highlights", "Ombre / Sombre / Color Melt", "Balayage", "Corrective Color"],
-        "Remedy + Restore" : ["Dry", "Oily", "Brittle / Fragile", "Rebellious / Curly", "Lifeless /  Dull", "Chemically Treated", "Thinning Hair", "Home Maintenance Systems Are Available"],
+        "Remedy+Restore" : ["Dry", "Oily", "Brittle / Fragile", "Rebellious / Curly", "Lifeless /  Dull", "Chemically Treated", "Thinning Hair", "Home Maintenance Systems Are Available"],
         "Polish": ["Special Event Hair", "Makeup Artistry", "Brow Wax", "Brow & Lash Tinting"],
         "Texture": ["Keratin Smoothing Treatment", "Relaxers", "Perms"],
         "Indulge" : ["Full Body Waxing & Sugaring", "Full Face Waxing & Sugaring", "Brow Microblading"]
