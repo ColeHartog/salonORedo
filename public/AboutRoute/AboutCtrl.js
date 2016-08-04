f947a5456f3c05ebe13061e460f9c3ef9edf0216
ColeHartog <agentcole7@gmail.com>
2016-08-04 13:16:35 -0600

    clean up, font choosing

--- public/AboutRoute/AboutCtrl.js
+++ public/AboutRoute/AboutCtrl.js
@@ -2,12 +2,12 @@ angular.module("salono").controller("AboutCtrl", ["$scope", "AboutSvc", function
 
 	$scope.Options = [
         {
-            name: "Our Vision",
+            name: "OUR VISION",
             info: "We strive to create a warm environment where our inspiration and talent can thrive. Our focus is to deliver a finished product matched specific to each guest’s own uniqueness.",
             info2: "Traditional feel. Contemporary product."
         },
         {
-            name: "Policies",
+            name: "POLICIES",
             info: "Forms of payments accepted are cash, checks and credit cards (VISA, MasterCard, Discover, American Express, and Debit Cards). We do not accept personal checks for first time guests. Please note that a deposit may be required at the time of booking. All prices are subject to change at the discretion of salon management and/or stylists. All prices and services vary by stylist. Cancellation of appointments must be made 24 hours prior to the scheduled time to avoid cancellation fees. For same day appointments, a three-hour cancellation notice is required. Please note a 72-hour cancellation notice is required for extension and retexturizing services. Failure to follow the salon’s cancellation policies will result in a forfeiture of deposit. Salon O does not offer a refund on services or product. If a guest is dissatisfied with their service we offer a complimentary adjustment if scheduled within seven days of the original appointment. If a guest is dissatisfied with a product we will gladly exchange it within 30 days. Salon O is not responsible for any damaged clothing, please dress accordingly. Please be courteous to other salon guests and stylists by not bringing children or additional guests."
         }];
     
