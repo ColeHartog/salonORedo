59573845bfdc75a8c6cb73045ee36894a5ab1b5e
ColeHartog <agentcole7@gmail.com>
2016-08-05 12:07:57 -0600

    image updates

--- public/js/mainCtrl.js
+++ public/js/mainCtrl.js
@@ -2,13 +2,13 @@ angular.module("salono").controller("mainCtrl", ["$scope", "mainSvc", function($
     
     $scope.imagesArray = [
         "./images/0296E%20A4%201920.jpg",
-        "./images/2236E%20A2v2%201920.jpg",
+        "./images/2236E%20A3%201920.jpg",
         "./images/1323E%20A3%201920.jpg",
         "./images/0346E%20A1%201920.jpg",
-        "./images/0453E%20A1%201920Flip.jpg",
+        "./images/0453E%20A2%201920Flip.jpg",
         "./images/0421E%20A1%201920.jpg",
         "./images/0445E%20A2w%201920.jpg",
-        "./images/0732E%20A1%201920.jpg"
+        "./images/0732E%20A2%201920.jpg"
     ];
     
     var index = 2;
