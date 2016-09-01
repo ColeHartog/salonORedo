6b49d0a49def6455e00812bfafe6245f0d834c99
ColeHartog <agentcole7@gmail.com>
2016-09-01 17:21:08 -0600

    mini

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require("gulp"),
-	sass = require("gulp-sass");
+	sass = require("gulp-sass"),
+    cleanCSS = require("gulp-clean-css");
 
 gulp.task("sass", function(){
 	return gulp.src("./public/styles.scss")
 		.pipe(sass())
 		.pipe(gulp.dest("./public"))
+});
+
+gulp.task("minify-css", function(){
+    return gulp.src('styles/*.css')
+    .pipe(cleanCSS({}))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task("watch", function(){
